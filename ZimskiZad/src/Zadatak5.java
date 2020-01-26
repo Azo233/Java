3e91bc9a326eed414d00f921653dd12f8c3d73ee
Andrija Azinić <57678257+Azo233@users.noreply.github.com>
2020-01-26 21:57:25 +0100

    Commit 3

--- ZimskiZad/src/Zadatak5.java
+++ ZimskiZad/src/Zadatak5.java
@@ -8,67 +8,64 @@ public class Zadatak5 {
 		int y=Integer.parseInt(JOptionPane.showInputDialog("Unesite 2. broj "));
 		
 		
+		int minR=0;
+		int maxR=x-1;
+		int minS=0;
+		int maxS=y-1;
+		int br=1;
 		
-		int mat [] []=new int [x][y];
+		int m[] []=new int [x][y];
 		
-		int pocReda=0;
-		int krajReda=y-1;
-		int pocStupca=0;
-		int krajStupca=x-1;
-		int count=1;
-		
+
+		while(true) {
+			 if(minS>maxS) {
+				 break;
+			 }
+
+		 for(int i=maxS;i>=minS;i--){
+			 m[maxR][i]=br++;
+		 }
+		 maxR--;
+		 
+		 if(minR>maxR) {
+			 break;
+		 }
+
+		 for(int i=maxR;i>=minR;i--){
+			 m[i][minS]=br++;
+		 }
+		 minS++;
+		 
+		 if(minS>maxS) {
+			 break;
+		 }
+
+		 for(int i=minS;i<=maxS;i++){
+			 m[minR][i]=br++;
+		 }
+		 minR++;
+		 
+		 if(minR>maxR) {
+			 break;
+		 }
+		 
+		 for(int i=minR;i<=maxR;i++){
+			 m[i][maxS]=br++;
+
+		 }
+		 maxS--;
+		 }
+			
+
 		
 			
-		for(int i=krajReda;i>=pocReda;i--) {
-			
-			mat[krajStupca][i]=count++;
-		}
-		
-		 for (int i=krajReda;i>=pocReda;i--) {
-			 
-			 mat[i][pocStupca]=count++;
-			 
-			 
-		 }
-		 
-		 for(int i=pocReda+1;i<=krajReda;i++) {
-			 mat[pocReda][i]=count++;
-		 }
-		 
-		 for(int i=pocStupca+1;i<krajStupca-1;i++) {
-			 mat[i][krajReda]=count++;
-			 
-		 }
-		 
-		 for(int i=krajReda;i>=pocReda+1;i--) {
-				
-				mat[krajStupca-1][i]=count++;
-				}
-		 
-		 
-		 for (int i=krajReda-1;i>=pocReda+1;i-- ) {
-			 mat[i][pocStupca+1]=count++;
-		 }
-		 
-		 for(int i=pocReda+1;i<=pocReda+1;i++) {
-		 mat[pocReda+1][krajStupca-2]=count++;
-		 }
-		 
-		 for (int i=krajReda-1;i==krajReda-1;i--) {
-			 mat[krajReda-1][krajStupca-2]=count++;
-		 }
-		 
-		 
-		 
-		
-				
-		
+ 
 		
 		for (int i = 0; i < x; i++)
 	    {
 	        for (int j = 0; j <y; j++)
 	        {
-	            System.out.print(mat[i][j]+ "\t");
+	            System.out.print(m[i][j]+ "\t");
 	        }
 	         
 	        System.out.println();
